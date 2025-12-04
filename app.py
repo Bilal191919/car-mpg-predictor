2eea0fb9c36f7ea329206cf439fcf244e7047462
Hafiz Bilal Sarwar <hafizbilal1919@gmail.com>
2025-12-04 21:12:25 +0500

    Create app.py file for Car MPG Predictor

---
+++ app.py
@@ -0,0 +1,19 @@
+# Car MPG Predictor
+
+This is a simple Python application to predict the miles per gallon (MPG) of cars based on certain features.
+
+## Features
+- Engine Size
+- Horsepower
+- Weight
+- Year
+
+## How to Run
+1. Install the required libraries.
+2. Run the application using `python app.py`.
+
+## License
+MIT License
+
+## Author
+Bilal191919
